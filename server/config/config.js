90e8f9eea710ff4e863cad852f05dcffbfc1dcab
Carlos Antonio Sonora <sonorac@mx1.ibm.com>
2018-10-30 12:17:44 -0600

    REST Server - usuario

---
+++ server/config/config.js
@@ -0,0 +1,20 @@
+//================================================
+//  Puerto
+//================================================
+process.env.PORT = process.env.PORT || 3000;
+//================================================
+//  Entorno
+//================================================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+//================================================
+//  Base de datos
+//================================================
+let urlDB;
+
+if(process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb: //localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb://casonorac:N0d3CoUrSe89uD3mY@ds145573.mlab.com:45573/cafe'
+}
+process.env.URLDB = urlDB;
+
