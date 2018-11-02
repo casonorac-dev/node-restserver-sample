ef29b3707dfb6ae7841ae59e43444e87b8b21665
Carls Antonio Sonora <carlos.sonora.jr@gmail.com>
2018-11-02 12:25:52 -0600

    Up to date with remote

--- server/config/config.js
+++ server/config/config.js
@@ -1,9 +1,6 @@
 //================================================
 //  Puerto
 //================================================
-<<<<<<< HEAD
-process.env.PORT = process.env.PORT || 3000;
-=======
 process.env.PORT = process.env.PORT || 3000;
 //================================================
 //  Entorno
@@ -13,11 +10,9 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 //  Base de datos
 //================================================
 let urlDB;
-if(process.env.NODE_ENV === 'dev') {
+if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb: //localhost:27017/cafe';
 } else {
     urlDB = process.env.MONGO_URI;
 }
-process.env.URLDB = urlDB; 
-
->>>>>>> c32407a456402cf3630148d20e72a4283d0a5dd9
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -1,12 +1,8 @@
 require('./config/config');
-<<<<<<< HEAD
-const express = require('express');
-=======
 
 const express = require('express');
 const mongoose = require('mongoose');
 
->>>>>>> c32407a456402cf3630148d20e72a4283d0a5dd9
 const app = express();
 
 const bodyParser = require('body-parser');
@@ -15,47 +11,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
 app.use(bodyParser.json());
-<<<<<<< HEAD
-
-
-app.get('/usuario', function(req, res) {
-    res.json('getUsuario')
-});
-
-app.post('/usuario', function(req, res) {
-    let body = req.body;
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-
-        });
-    } else {
-        res.json({ persona: body });
-    }
-});
-
-app.put('/usuario/:id', function(req, res) {
-    let id = req.params.id;
-    res.json({ id });
-});
-
-app.delete('/usuario', function(req, res) {
-    res.json('deleteUsuario')
-});
-
-app.listen(process.env.PORT, () => {
-    console.log('Escuchando puerto: ', 3000);
-=======
 app.use(require('./routes/usuario'));
 
 mongoose.connect(process.env.URLDB, (err, res) => {
-    if(err) throw err;
+    if (err) throw err;
 
     console.log('Base de datos Online');
 });
 
 app.listen(process.env.PORT, () => {
     console.log('Escuchando puerto: ', process.env.PORT);
->>>>>>> c32407a456402cf3630148d20e72a4283d0a5dd9
 });
